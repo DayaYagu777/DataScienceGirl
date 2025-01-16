ac859efc72433be9537fb967ae605dfcb709e642
DayaYagu777 <dayayagu@ucm.es>
2025-01-16 21:13:03 +0100

    Update terminal_clean.py

--- terminal_clean.py
+++ terminal_clean.py
@@ -2,12 +2,31 @@ import os
 import time
 
 def limpiar_terminal():
+    """
+    Limpia la terminal de comandos.
+    
+    Esta función utiliza el comando adecuado según el sistema operativo:
+    'cls' para Windows y 'clear' para sistemas Unix/Linux.
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def limpiar_terminal_1s():
-    time.sleep(1.5)
-    os.system('cls' if os.name == 'nt' else 'clear')
+    """
+    Limpia la terminal después de esperar 1.5 segundos.
+    
+    Esta función espera 1.5 segundos antes de limpiar la terminal,
+    permitiendo que los mensajes sean visibles brevemente antes de ser eliminados.
+    """
+    time.sleep(1.5)  # Espera 1.5 segundos
+    os.system('cls' if os.name == 'nt' else 'clear')  # Limpia la terminal
 
 def limpiar_terminal_2s():
-    time.sleep(2.5)
-    os.system('cls' if os.name == 'nt' else 'clear')
+    """
+    Limpia la terminal después de esperar 2.5 segundos.
+    
+    Esta función espera 2.5 segundos antes de limpiar la terminal,
+    permitiendo que los mensajes sean visibles brevemente antes de ser eliminados.
+    """
+    time.sleep(2.5)  # Espera 2.5 segundos
+    os.system('cls' if os.name == 'nt' else 'clear')  # Limpia la terminal
+
