8f6683c62c50255c85a8cac6e19b2db249beedc0
DayaYagu777 <dayayagu@ucm.es>
2025-01-16 18:49:16 +0100

    Update terminal_clean.py

--- terminal_clean.py
+++ terminal_clean.py
@@ -5,13 +5,9 @@ def limpiar_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def limpiar_terminal_1s():
-    # Esperar 10 segundos para que se vean los mensajes
     time.sleep(1.5)
-    # Limpiar la terminal
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def limpiar_terminal_2s():
-    # Esperar 10 segundos para que se vean los mensajes
     time.sleep(2.5)
-    # Limpiar la terminal
     os.system('cls' if os.name == 'nt' else 'clear')
